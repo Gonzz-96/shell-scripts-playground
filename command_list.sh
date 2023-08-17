735bfbc93f3457d337e61bc5038a95472ec764f4
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-08-17 15:17:23 -0600

    Subshell scripts.

---
+++ command_list.sh
@@ -0,0 +1,13 @@
+# This a command list does not create a new subshell.
+# If a subshell is created, the $BASH_SUBSHELL environment
+# variable is set to 1.
+
+is_subshell() {
+    if [ 1 -eq $BASH_SUBSHELL ]; then
+        echo "A subshell was created"
+    else
+        echo "No subshell was created"
+    fi 
+}
+
+ls; echo "Hello, world"; ps -f; is_subshell
